d031fac0390d67472d7ae04244564ecbbdf7a03c
Andre Passaes <andrepassaes@hotmail.com>
2024-02-18 21:19:42 -0300

    Implement admin flow for dashboard

--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Team, Player
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'grade', 'is_goalkeeper', 'team')
+
 
 # Register your models here.
